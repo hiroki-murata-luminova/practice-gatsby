32f48d7db697ef462e72c2c70aaa3ea3afc49711
hiroki.murata <hiroki.murata@luminova.co.jp>
2021-06-01 16:38:26 +0900

    firstcommit

---
+++ src/pages/about.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "gatsby";
+
+const AboutPage = () => (
+  <div>
+    <p>About</p>
+    <Link to="/">Topへ戻る</Link>
+  </div>
+);
+
+export default AboutPage;
